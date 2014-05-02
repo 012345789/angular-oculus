bc151cab2084276ddd15623e45d05d5cd545c5c3
G4 <g4@ubuntu.ubuntu-domain>
2014-05-01 17:02:42 -0700

    Dynamically loaded video and previous and next video thumbnails work

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
+  'ngAnimate',
   'ngSanitize',
   'ngRoute',
   'myApp.filters',
@@ -10,12 +11,14 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+config(['$routeProvider', '$locationProvider', '$sceDelegateProvider', function($routeProvider, $locationProvider, $sceDelegateProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MainVideoController'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  //{{:videoID}}?
   $routeProvider.when('/Video/:videoID', {templateUrl: 'partials/currentVideo.html', controller: 'VideoController', controllerAs: 'video'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'https://www.youtube.com/**']);
   //$locationProvider.html5Mode(true);
 }]);//.
 // directive('youtubePlayer', function($sce) {
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,15 +12,39 @@ angular.module('myApp.controllers', [])
       this.$route = $route;
       this.$location = $location;
       this.$routeParams = $routeParams;
-      $scope.videoCode = 'LqOfPkHGq9U'; //for testing
   }])
 
-  .controller('VideoController', ['$routeParams', '$scope', '$sce', function($routeParams, $scope, $sce) {
+  .controller('VideoController', ['$routeParams', '$scope', '$sce', '$http', function($routeParams, $scope, $sce, $http) {
     this.name = "VideoController";
-    //this.params = $routeParams;  un-needed
+    this.params = $routeParams;
+    var apiKey = 'AIzaSyDy4TuYNtqRGCENOikEs-20m3zBWw0yE3Q'; //this key is unique to my IP address. if you need to test this on your computer, please give me your IP address so I can allow you access
+    
+    $scope.whereToStart = function(pageToken) {
+      if($scope.nextPageToken || $scope.prevPageToken) {
+        return 1;//someToken;
+      } else {
+        return "";
+      }
+    };
+
+    $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=oculusvr&maxResults=1&key=' + apiKey).success(function(data) {
+      $scope.videoList = data.items;
+      $scope.nextPageToken = data.nextPageToken;
+      $scope.prevPageToken = data.prevPageToken;
+      $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=oculusvr&maxResults=1&pageToken=' + $scope.nextPageToken + '&key=' + apiKey).success(function(data) {
+        $scope.nextVideoThumbnail = data.items[0].snippet.thumbnails.default.url;
+      });
+      $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=oculusvr&maxResults=1&pageToken=' + $scope.prevPageToken + '&key=' + apiKey).success(function(data) {
+        $scope.prevVideoThumbnail = data.items[0].snippet.thumbnails.default.url;
+      });
+    });
+
+    $scope.getIframeSrc = function(videoId) {
+      return 'https://www.youtube.com/embed/' + videoId;
+    };
     //$scope.videoLink2 = $routeParams.videoID;  un-needed
     $scope.fullVideoLink = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + this.params.videoID);
-    //^ IT FINALLY WORKS ^
+    //$scope.trustedVideoList = $sce.trustAsHtml($scope.videoList);
     //$scope.videoLink = $sce.trustAsResourceUrl(params.videoID);
   }])
 
