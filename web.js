75d0fe415f6f5fad0d782243bbbaef94d55d1b1a
G4 <holyduper@gmail.com>
2014-05-25 09:39:54 -0700

    Trying to deploy using Heroku 2

--- web.js
+++ web.js
@@ -9,6 +9,4 @@ app.get('/', function(req, res) {
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 5000);
