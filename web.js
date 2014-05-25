9acbc449053324751bcea83f99ff1488419621fa
G4 <holyduper@gmail.com>
2014-05-25 09:56:36 -0700

    Testing web.js deployment

--- web.js
+++ web.js
@@ -9,4 +9,6 @@ app.get('/', function(req, res) {
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
