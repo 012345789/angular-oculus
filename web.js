c8e11e9f0c2afff31501a1829d648a9ec3f4a265
G4 <holyduper@gmail.com>
2014-05-25 10:07:57 -0700

    Testing with Heroku deployment

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
